ee997f4d9e4ec9c51c97157e511d0c2caf4e58fe
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2025-12-10 18:23:26 +0900

    feat: キーボードイベントのリスニング機能を追加し、関連する依存関係を設定

--- lib/pages/editor_page/editor_page.dart
+++ lib/pages/editor_page/editor_page.dart
@@ -1,9 +1,10 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 // Package imports:
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:keyboard_event/keyboard_event.dart';
 
 // Project imports:
 import 'package:mc_hub/widgets/custom_appbar.dart';
@@ -12,25 +13,40 @@ class EditorPage extends HookConsumerWidget {
   const EditorPage({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    late final KeyboardEvent keyboardEvent;
+    final keyDownEvents = useState<List<String?>>([]);
+
+    Future<void> initKeyboard() async {
+      await KeyboardEvent.init(); // 仮想キー情報の初期化[web:1]
+      keyboardEvent.startListening((keyEvent) {
+        if (keyEvent.isKeyDown &&
+            !keyDownEvents.value.contains(keyEvent.vkName)) {
+          keyDownEvents.value = [...keyDownEvents.value, keyEvent.vkName];
+        } else {
+          keyDownEvents.value = [];
+        }
+        // debugPrint(keyEvent.toString());
+        debugPrint(keyDownEvents.value.join(" + "));
+      });
+    }
+
+    useEffect(() {
+      keyboardEvent = KeyboardEvent();
+      initKeyboard();
+      return () {
+        keyboardEvent.cancelListening();
+      };
+    }, []);
+
     return Scaffold(
       appBar: CustomAppbar().build(context, ref),
       body: Center(
-        // child: OutlinedButton(
-        //   onPressed: () {
-        //     Navigator.of(context).pop();
-        //   },
-        //   child: Text("こんにちは"),
-        // ),
-        // child: Focus(
-        //   autofocus: true,
-        //   onKeyEvent: (node, event) {
-        //     final key = event.logicalKey;
-        //     if (event is KeyDownEvent) print("DOWN:$key");
-        //     return KeyEventResult.handled;
-        //   },
-        //   child: Container(width: 300, height: 300, color: Colors.red),
-        // ),
-        
+        child: OutlinedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text(keyDownEvents.value.join(" + ")),
+        ),
       ),
     );
   }
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <bitsdojo_window_windows/bitsdojo_window_plugin.h>
+#include <keyboard_event/keyboard_event_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   BitsdojoWindowPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("BitsdojoWindowPlugin"));
+  KeyboardEventPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("KeyboardEventPlugin"));
 }
