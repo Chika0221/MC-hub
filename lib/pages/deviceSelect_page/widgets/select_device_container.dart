182c2de3720aaf86877e6d13ee2b4e070b9bf323
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2025-12-13 11:19:14 +0900

    refactor: 不要なコードをコメントアウトし、RiveWidgetBuilderの構造を整理

--- lib/pages/deviceSelect_page/widgets/select_device_container.dart
+++ lib/pages/deviceSelect_page/widgets/select_device_container.dart
@@ -9,7 +9,27 @@ class SelectDeviceContainer extends HookConsumerWidget {
       riveFactory: Factory.rive,
     );
 
+    // late final riveController;
+    // late final file;
+
+    // void initRive() async {
+    //   file = await File.asset(
+    //     "assets/animations/fondKeyboard.riv",
+    //     riveFactory: Factory.rive,
+    //   );
+
+    //   riveController = useMemoized(
+    //     () => RiveWidgetController(
+    //       file,
+    //       stateMachineSelector: StateMachineSelector.byIndex(0),
+    //     ),
+    //     [],
+    //   );
+    // }
+
     useEffect(() {
+      // initRive();
+
       return () {
         fileLoader.dispose();
       };
@@ -18,38 +38,27 @@ class SelectDeviceContainer extends HookConsumerWidget {
     return GlassContainer.clearGlass(
       width: MediaQuery.of(context).size.height * 0.7,
       height: MediaQuery.of(context).size.height * 0.6,
-      // decoration: BoxDecoration(
-      //   borderRadius: BorderRadius.circular(10),
-      //   color: Theme.of(context).colorScheme.surfaceContainer,
-      //   border: Border.all(
-      //     color: Theme.of(context).colorScheme.primary,
-      //     width: 1,
-      //   ),
-      // ),
-      // borderRadius: BorderRadius.circular(10),
       borderRadius: BorderRadius.circular(10),
-      child: InkWell(
-        borderRadius: BorderRadius.circular(10),
-        onTap: () {
-          Navigator.of(context).pushNamed(AppRoute.editor.path);
-        },
-        child: RiveWidgetBuilder(
-          fileLoader: fileLoader,
-          builder:
-              (context, state) => switch (state) {
-                RiveLoading() => const Center(
-                  child: CircularProgressIndicator(),
-                ),
-                RiveFailed() => ErrorWidget.withDetails(
-                  message: state.error.toString(),
-                  error: FlutterError(state.error.toString()),
-                ),
-                RiveLoaded() => RiveWidget(
+      child: RiveWidgetBuilder(
+        fileLoader: fileLoader,
+        builder:
+            (context, state) => switch (state) {
+              RiveLoading() => const Center(child: CircularProgressIndicator()),
+              RiveFailed() => ErrorWidget.withDetails(
+                message: state.error.toString(),
+                error: FlutterError(state.error.toString()),
+              ),
+              RiveLoaded() => InkWell(
+                onTap: () async {
+                  await Future.delayed(Duration(milliseconds: 100));
+                  Navigator.of(context).pushNamed(AppRoute.editor.path);
+                },
+                child: RiveWidget(
                   controller: state.controller,
                   fit: Fit.contain,
                 ),
-              },
-        ),
+              ),
+            },
       ),
     );
   }
