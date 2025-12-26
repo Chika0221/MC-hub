409a1972027f8ba4aa9b32cb3c329d5b91c00ce8
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2025-12-27 00:25:35 +0900

    改善: アプリケーションアイコンとトレイアイコンを追加し、flutter_launcher_iconsを設定

--- lib/models/my_device.dart
+++ lib/models/my_device.dart
@@ -1,6 +1,5 @@
 // Package imports:
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:json_annotation/json_annotation.dart';
 
 // Project imports:
 import 'package:mc_hub/pages/editor_page/key_data.dart';
