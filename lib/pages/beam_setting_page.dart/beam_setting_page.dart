333c29e008035ed46ed96cfa224610c4c3a89084
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2025-12-31 00:31:47 +0900

    fix: BeamSettingPageのUIを更新し、BeamItemListViewを追加してリスト表示を実装

--- lib/pages/beam_setting_page.dart/beam_setting_page.dart
+++ lib/pages/beam_setting_page.dart/beam_setting_page.dart
@@ -5,22 +5,45 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 // Project imports:
-import 'package:mc_hub/theme/custom_theme.dart';
+import 'package:mc_hub/pages/beam_setting_page.dart/widgets/beam_item_list_view.dart';
+import 'package:mc_hub/widgets/custom_appbar.dart';
 
 class BeamSettingPage extends HookConsumerWidget {
   const BeamSettingPage({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: Theme.of(context).colorScheme.primary,
-        child: Center(
-          child: Text(
-            "--MAYUZAWA--",
-            style: CustomTheme().titleTheme(context).textTheme.displayLarge,
-          ),
+      appBar: CustomAppbar(isShowTitle: false).build(context, ref),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          spacing: 16,
+          children: [
+            BeamItemListView(
+              backgroundColor: Theme.of(context).colorScheme.tertiary,
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return Container(
+                  height: 100,
+                  width: double.infinity,
+                  color: Theme.of(context).colorScheme.primary,
+                );
+              },
+            ),
+            BeamItemListView(
+              backgroundColor: Theme.of(
+                context,
+              ).colorScheme.tertiary.withAlpha((255 * 2 / 3).toInt()),
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return Container(
+                  height: 100,
+                  width: double.infinity,
+                  color: Theme.of(context).colorScheme.primary,
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
