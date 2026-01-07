66d94bdcfbfb20642678a2dc6ab0bdc3e5b7386d
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2026-01-07 15:20:39 +0900

    feat: 不要なインポートを削除し、コードをクリーンアップ

--- lib/pages/editor_page/widgets/macro_setting/macro_setting_dialog.dart
+++ lib/pages/editor_page/widgets/macro_setting/macro_setting_dialog.dart
@@ -4,9 +4,6 @@ import 'package:flutter/material.dart';
 // Package imports:
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-// Project imports:
-import 'package:mc_hub/widgets/custom_full_page_dialog.dart';
-
 class MacroSettingDialog extends HookConsumerWidget {
   const MacroSettingDialog({super.key, required this.title});
 
