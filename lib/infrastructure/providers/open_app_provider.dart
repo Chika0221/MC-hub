06f9cd791f23337e83a71f0e732129b104097991
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2026-01-18 00:34:05 +0900

    feat: アプリ情報モデルを追加し、開いているアプリを取得する機能を実装

--- lib/infrastructure/providers/open_app_provider.dart
+++ lib/infrastructure/providers/open_app_provider.dart
@@ -1,11 +1,14 @@
 // Dart imports:
 import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 
 // Package imports:
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:window_watcher/window_watcher.dart';
 
 // Project imports:
+import 'package:mc_hub/models/app_info.dart';
 import 'package:mc_hub/models/macro.dart';
 
 class OpenAppNotifier extends AsyncNotifier<List<Macro>> {
@@ -22,6 +25,23 @@ class OpenAppNotifier extends AsyncNotifier<List<Macro>> {
   }
 
   Future<List<Macro>> getOpenApps() async {
+    final result = await Process.run('powershell', [
+      'Get-ItemProperty',
+      r'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*, HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*, HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayIcon | Where-Object {$_.DisplayName -ne $null -and $_.DisplayIcon -ne $null} | ConvertTo-Json',
+    ]);
+
+    final out = (result.stdout ?? "").toString();
+
+    final List<dynamic> listJson = jsonDecode(out);
+
+    final List<AppInfo> apps =
+        listJson.map((e) => AppInfo.fromJson(e)).toList();
+
+    apps.forEach((app) {
+      print('${app.DisplayName} -> ${app.DisplayIcon}');
+    });
+    print(apps.length);
+
     final windowList = await WindowWatcher.getWindows(getExe: true);
     final macros =
         windowList.map((window) {
---
+++ lib/models/app_info.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_info.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$AppInfoImpl _$$AppInfoImplFromJson(Map<String, dynamic> json) =>
+    _$AppInfoImpl(
+      DisplayName: json['DisplayName'] as String,
+      DisplayIcon: json['DisplayIcon'] as String,
+    );
+
+Map<String, dynamic> _$$AppInfoImplToJson(_$AppInfoImpl instance) =>
+    <String, dynamic>{
+      'DisplayName': instance.DisplayName,
+      'DisplayIcon': instance.DisplayIcon,
+    };
