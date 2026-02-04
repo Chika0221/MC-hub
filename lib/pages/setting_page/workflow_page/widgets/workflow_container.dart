774520997fded39ceb95c272710b091dc9e99fdf
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2026-02-04 14:43:33 +0900

    feat: コンテキストメニューを追加し、ワークフロー削除機能を実装

--- lib/pages/setting_page/workflow_page/widgets/workflow_container.dart
+++ lib/pages/setting_page/workflow_page/widgets/workflow_container.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 
 // Package imports:
+import 'package:flutter_context_menu/flutter_context_menu.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 // Project imports:
@@ -9,6 +10,7 @@ import 'package:mc_hub/infrastructure/providers/firebase_workflow_provider.dart'
 import 'package:mc_hub/infrastructure/providers/workflow_edit_provider.dart';
 import 'package:mc_hub/infrastructure/workflow/run_workflow.dart';
 import 'package:mc_hub/models/workflow.dart';
+import 'package:mc_hub/pages/setting_page/workflow_page/widgets/workflow_context.dart';
 import 'package:mc_hub/pages/setting_page/workflow_page/workflow_edit_page.dart';
 
 class WorkflowContainer extends HookConsumerWidget {
@@ -20,77 +22,96 @@ class WorkflowContainer extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final isTimeBasedTrigger = workflow.trigger.type == TriggerType.TimeBased;
 
-    return AnimatedContainer(
-      duration: Duration(milliseconds: 200),
-      decoration: ShapeDecoration(
-        shape: StadiumBorder(),
-        color:
-            (workflow.state)
-                ? Theme.of(context).colorScheme.secondary
-                : Theme.of(context).colorScheme.surface,
+    final entry = <ContextMenuEntry>[
+      CustomContextMenuEntry(
+        label: Text('ワークフローを削除'),
+        icon: Icon(Icons.delete_rounded),
+        onSelected: (value) {
+          ref.read(firebaseWorkflowProvider.notifier).deleteWorkflow(workflow);
+        },
       ),
-      padding: EdgeInsets.fromLTRB(24, 8, 16, 8),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Expanded(
-            child: InkWell(
-              onTap: () async {
-                ref
-                    .watch(workflowEditInitialWorkflowProvider.notifier)
-                    .updateInitialWorkflow(workflow);
+    ];
 
-                final Workflow? returnWorkflow = await Navigator.of(
-                  context,
-                ).push(
-                  MaterialPageRoute(builder: (context) => WorkflowEditPage()),
-                );
-
-                if (returnWorkflow != null) {
+    return ContextMenuRegion(
+      contextMenu: ContextMenu(
+        boxDecoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Theme.of(context).colorScheme.onSurface,
+        ),
+        entries: entry,
+      ),
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 200),
+        decoration: ShapeDecoration(
+          shape: StadiumBorder(),
+          color:
+              (workflow.state)
+                  ? Theme.of(context).colorScheme.secondary
+                  : Theme.of(context).colorScheme.surface,
+        ),
+        padding: EdgeInsets.fromLTRB(24, 8, 16, 8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: InkWell(
+                onTap: () async {
                   ref
-                      .read(firebaseWorkflowProvider.notifier)
-                      .updateWorkflow(returnWorkflow);
-                }
-              },
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    workflow.displayName,
-                    overflow: TextOverflow.ellipsis,
-                    style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                      color:
-                          (workflow.state)
-                              ? Theme.of(context).colorScheme.onSecondary
-                              : Theme.of(context).colorScheme.onSurface,
+                      .watch(workflowEditInitialWorkflowProvider.notifier)
+                      .updateInitialWorkflow(workflow);
+
+                  final Workflow? returnWorkflow = await Navigator.of(
+                    context,
+                  ).push(
+                    MaterialPageRoute(builder: (context) => WorkflowEditPage()),
+                  );
+
+                  if (returnWorkflow != null) {
+                    ref
+                        .read(firebaseWorkflowProvider.notifier)
+                        .updateWorkflow(returnWorkflow);
+                  }
+                },
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      workflow.displayName,
+                      overflow: TextOverflow.ellipsis,
+                      style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                        color:
+                            (workflow.state)
+                                ? Theme.of(context).colorScheme.onSecondary
+                                : Theme.of(context).colorScheme.onSurface,
+                      ),
                     ),
-                  ),
-                  Text(
-                    "${workflow.trigger.type.name} トリガー | アクション数: ${workflow.actions.length}",
-                    overflow: TextOverflow.ellipsis,
-                    style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                      color:
-                          (workflow.state)
-                              ? Theme.of(context).colorScheme.onSecondary
-                              : Theme.of(context).colorScheme.onSurface,
+                    Text(
+                      "${workflow.trigger.type.name} トリガー | アクション数: ${workflow.actions.length}",
+                      overflow: TextOverflow.ellipsis,
+                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                        color:
+                            (workflow.state)
+                                ? Theme.of(context).colorScheme.onSecondary
+                                : Theme.of(context).colorScheme.onSurface,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          if (!isTimeBasedTrigger) ...[
-            SizedBox(width: 16),
-            IconButton.filled(
-              icon: Icon(Icons.play_arrow_rounded),
-              onPressed: () {
-                WorkflowService(workflow: workflow).runWorkflow();
-              },
-            ),
+            if (!isTimeBasedTrigger) ...[
+              SizedBox(width: 16),
+              IconButton.filled(
+                icon: Icon(Icons.play_arrow_rounded),
+                onPressed: () {
+                  WorkflowService(workflow: workflow).runWorkflow();
+                },
+              ),
+            ],
           ],
-        ],
+        ),
       ),
     );
   }
