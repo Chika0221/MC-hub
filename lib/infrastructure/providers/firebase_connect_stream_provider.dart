f835dd0ecc8402cee4f1b5a6b6140a211108f806
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2026-02-25 18:33:27 +0900

    feat: FirebaseConnectStreamNotifierにデバイス情報を追加し、connectコレクションへのデータ追加を実装

--- lib/infrastructure/providers/firebase_connect_stream_provider.dart
+++ lib/infrastructure/providers/firebase_connect_stream_provider.dart
@@ -2,11 +2,32 @@
 import 'package:flutter/material.dart';
 
 // Package imports:
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-// class FirebaseConnectStreamNotifier extends StreamNotifier<> {
-//   @override
-//   Stream<> build() {
-    
-//   }
-// }
+// Project imports:
+import 'package:mc_hub/models/connect.dart';
+
+class FirebaseConnectStreamNotifier extends StreamNotifier<Connect> {
+  final connect_collection = FirebaseFirestore.instance.collection("connect");
+
+  @override
+  Stream<Connect> build() async* {
+    final deviceInfo = await DeviceInfoPlugin().windowsInfo;
+
+    await connect_collection.add(
+      Connect(
+        host: ConnectHost(
+          hostID: deviceInfo.deviceId,
+          hostName: deviceInfo.computerName,
+        ),
+        controllerID: null,
+      ).toJson(),
+    );
+
+    // return connect_collection.snapshots().map((e) {
+    //   return e.docs.map((e) => Connect.fromJson(e.data())).toList();
+    // });
+  }
+}
